39eb67b67beed0ebc2840a208e9b3fa370515338
JungBum <109258397+BumMeKR@users.noreply.github.com>
2022-08-12 12:15:03 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -2,6 +2,5 @@ def solution(s):
     en_numbers_list = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
     for idx, en_num in enumerate(en_numbers_list):
-        if en_num in s:
-            s = s.replace(en_num, str(idx))
+        s = s.replace(en_num, str(idx))
     return int(s)
