ea676048c9ea95e0ef65c0937bc91289dd33a0ca
JungBum <109258397+BumMeKR@users.noreply.github.com>
2022-08-12 12:14:23 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,8 +1,7 @@
 string = input()
 
 cro_list = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
-cnt = 0
+
 for char in cro_list:
-    if string.count(char):
-        string = string.replace(char, " ")
+    string = string.replace(char, " ")
 print(len(string))
