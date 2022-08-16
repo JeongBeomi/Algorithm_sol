6bb22f978565be252f49c2554d02ab58edf07bb6
JungBum <109258397+BumMeKR@users.noreply.github.com>
2022-08-16 13:27:15 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 3,456 ms, Memory: 75,436 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -10,12 +10,14 @@ for _ in range(10):
             break
         # 가로 탐색 길이 i인 회문 검사
         for string_line in string_list:
-            for j in range(0, 100 - i + 1):
+            for j in range(100 - i + 1):
                 if string_line[j:j + i] == string_line[j:j + i][::-1]:
                     max_cnt = i
+
         # 세로 탐색 길이 i인 회문 검사
         for new_string_line in zip(*string_list):
             for k in range(0, 100 - i + 1):
                 if new_string_line[k:k + i] == new_string_line[k:k + i][::-1]:
                     max_cnt = i
+                    
     print(f"#{tc} {max_cnt}")
