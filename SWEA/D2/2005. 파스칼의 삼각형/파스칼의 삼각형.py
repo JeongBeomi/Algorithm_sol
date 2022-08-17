df9f2624ab556f3103e0395d779147cad6ab0b3e
JungBum <109258397+BumMeKR@users.noreply.github.com>
2022-08-17 17:43:31 +0900

    [D2] Title: 파스칼의 삼각형, Time: 144 ms, Memory: 56,952 KB -BaekjoonHub

---
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -0,0 +1,15 @@
+def pascal(n):
+    if n > len(memo):
+        add_line = [1]
+        for i in range(n-2):
+            add_line.append(memo[n-2][i]+memo[n-2][i+1])
+        add_line.append(1)
+        memo.append(add_line)
+    return memo
+memo = [[1]]
+t = int(input())
+for tc in range(t):
+    n = int(input())
+    print(f"#{tc+1}")
+    for k in pascal(n):
+        print(*k)
