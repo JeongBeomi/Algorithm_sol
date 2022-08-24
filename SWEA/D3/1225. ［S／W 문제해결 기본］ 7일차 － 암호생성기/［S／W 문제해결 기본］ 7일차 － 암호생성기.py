35ca12dcbbae2716909e4d8e989b6ee25c45734d
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-08-25 00:39:50 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 154 ms, Memory: 60,360 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -2,10 +2,11 @@ def secret(q):
     while True:
         for i in range(1,6):
             num = q.pop(0)
-            if num - (i) <= 0:
+            if num - i <= 0:
                 q.append(0)
                 return
             q.append(num - i)
+
 
 for _ in range(10):
     n = int(input())
