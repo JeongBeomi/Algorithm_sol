d895fdd40b722e605b82526a680026d94408d99c
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-08-24 17:05:14 +0900

    [D2] Title: 파리 퇴치, Time: 151 ms, Memory: 59,568 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,15 +1,15 @@
 t = int(input())
-
 for tc in range(t):
     n, m = map(int, input().split())
-    map_list = [list(map(int, input().split())) for _ in range(n)]
+    total_map = [list(map(int, input().split())) for _ in range(n)]
     max_cnt = 0
     for i in range(n - m + 1):
         for j in range(n - m + 1):
             cnt = 0
-            for k in range(m):
-                for p in range(m):
-                    cnt += map_list[i + k][j + p]
+            for row in range(m):
+                for col in range(m):
+                    cnt += total_map[i + row][j + col]
+
             if max_cnt < cnt:
                 max_cnt = cnt
 
