4b3007e0ba49b185174a1c163d9ca3b04fc304f8
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-08-26 11:48:25 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 3,085 ms, Memory: 64,628 KB -BaekjoonHub

---
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
@@ -0,0 +1,22 @@
+t = int(input())
+for tc in range(t):
+    n = int(input())
+    numbers_list = list(map(int, input().split()))
+    danjo = []
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            num = numbers_list[i] * numbers_list[j]
+            if num <= 10:
+                break
+
+            num_list = list(map(int, str(num)))
+            for k in range(len(num_list) - 1):
+                if num_list[k] > num_list[k + 1]:
+                    break
+            else:
+                danjo.append(num)
+
+    if not danjo:
+        print(f"#{tc + 1} -1")
+    else:
+        print(f"#{tc + 1} {max(danjo)}")
