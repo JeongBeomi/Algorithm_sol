51fef79249332454ccf134a7f8badd13bea95a93
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-08-26 21:04:36 +0900

    [D2] Title: 지그재그 숫자, Time: 130 ms, Memory: 56,692 KB -BaekjoonHub

--- SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
@@ -1,10 +1,11 @@
 t = int(input())
 for tc in range(t):
     n = int(input())
-    result = 0
-    for j in range(1, n + 1):
-        if j % 2 == 0:
-            result -= j
-        else:
-            result += j
+    result = (n + 1) // 2 * ((-1) ** ((n + 1) % 2))
+    # result = 0
+    # for j in range(1, n + 1):
+    #     if j % 2 == 0:
+    #         result -= j
+    #     else:
+    #         result += j
     print(f"#{tc + 1} {result}")
