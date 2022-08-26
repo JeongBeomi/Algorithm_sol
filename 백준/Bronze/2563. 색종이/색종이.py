2630032cf3836983fa7f04d808170c238fa6a7bf
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-08-26 16:40:23 +0900

    [Bronze I] Title: 색종이, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2563. 색종이/색종이.py
+++ 백준/Bronze/2563. 색종이/색종이.py
@@ -1,13 +1,11 @@
+board = [[0] * 100 for _ in range(100)]
+cnt = 0
 t = int(input())
-
-map_list = [[0] * 100 for _ in range(100)]
-overlap_cnt = 0
 for tc in range(t):
-    x, y = map(int, input().split())
-    for i in range(x, x + 10):
-        for j in range(y, y + 10):
-            if map_list[i][j]:
-                overlap_cnt += 1
-                continue
-            map_list[i][j] = 1
-print(f"{100 * t - overlap_cnt}")
+    r, c = map(int, input().split())
+    for i in range(r, r + 10):
+        for j in range(c, c + 10):
+            if board[i][j] == 0:
+                cnt += 1
+                board[i][j] = 1
+print(cnt)
