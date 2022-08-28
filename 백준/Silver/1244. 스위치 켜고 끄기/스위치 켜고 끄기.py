4c9b621ec710e228e95eb8df982e60092d759c3c
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-08-28 18:01:30 +0900

    [Silver III] Title: 스위치 켜고 끄기, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -0,0 +1,27 @@
+length = int(input())
+sw_state = list(map(int, input().split()))
+n = int(input())
+for _ in range(n):
+    gender, num = map(int, input().split())
+    # 남자
+    if gender == 1:
+        for i in range(length):
+            if (i + 1) % num == 0:
+                sw_state[i] = int(not sw_state[i])
+    # 여자
+    else:
+        sw_state[num - 1] = int(not sw_state[num - 1])
+        cnt = 1
+        while True:
+            l, r = num - 1 - cnt, num - 1 + cnt
+            if l < 0 or r >= length or sw_state[l] != sw_state[r]:
+                break
+            sw_state[l] = int(not sw_state[l])
+            sw_state[r] = sw_state[l]
+            cnt += 1
+
+
+for idx in range(1, length + 1):
+    print(sw_state[idx - 1], end=" ")
+    if idx % 20 == 0:
+        print()
