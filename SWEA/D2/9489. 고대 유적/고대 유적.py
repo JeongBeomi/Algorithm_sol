0def2255da4708cdd1fb807aa1dc49ba7c041957
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-08-29 11:08:31 +0900

    [D2] Title: 고대 유적, Time: 169 ms, Memory: 61,688 KB -BaekjoonHub

--- SWEA/D2/9489. 고대 유적/고대 유적.py
+++ SWEA/D2/9489. 고대 유적/고대 유적.py
@@ -1,29 +1,26 @@
 t = int(input())
 for tc in range(t):
-    n, m = map(int, input().split())
+    n, m  = map(int, input().split())
     total_map = [list(map(int, input().split())) for _ in range(n)]
-    max_size = 2
+
     max_cnt = 0
     for i in range(n):
         cnt = 0
-        if total_map[i].count(1) > max_size:
-            for j in range(m):
-                if total_map[i][j] == 1:
-                    cnt += 1
-                    if cnt > max_cnt:
-                        max_cnt = cnt
-                else:
-                    cnt = 0
-    total_map = list(zip(*total_map))
+        for j in range(m):
+            if total_map[i][j]:
+                cnt += 1
+                if max_cnt < cnt:
+                    max_cnt = cnt
+            else:
+                cnt = 0
 
     for i in range(m):
         cnt = 0
-        if total_map[i].count(1) > max_size:
-            for j in range(n):
-                if total_map[i][j] == 1:
-                    cnt += 1
-                    if cnt > max_cnt:
-                        max_cnt = cnt
-                else:
-                    cnt = 0
-    print(f"#{tc+1} {max_cnt}")
+        for j in range(n):
+            if total_map[j][i]:
+                cnt += 1
+                if max_cnt < cnt:
+                    max_cnt = cnt
+            else:
+                cnt = 0
+    print(f"#{tc + 1} {max_cnt}")
