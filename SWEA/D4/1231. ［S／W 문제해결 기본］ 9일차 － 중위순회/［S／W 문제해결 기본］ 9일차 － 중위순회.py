e32eb6b9813ee5ddbda6803962fce4fa0d8e4293
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-09-13 17:04:08 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 158 ms, Memory: 57,180 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.py
@@ -1,7 +1,7 @@
-def inorder(n):    # 중위 순회
+def inorder(n):    # 완전 이진트리 중위 순회
     if n <= N:
         inorder(2 * n)
-        print(tree[n], end="")    # visit(n)
+        print(tree[n], end="")
         inorder(2 * n + 1)
 
 
@@ -9,7 +9,7 @@ for tc in range(10):
     N = int(input())
     tree = [0] * 101
 
-    for _ in range(N):      # 완저 이진트리 만들기
+    for _ in range(N):      # 완전 이진트리 만들기
         input_list = list(input().split())
         tree[int(input_list[0])] = input_list[1]
 
