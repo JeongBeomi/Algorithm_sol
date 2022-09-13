aadfbdd23140177b8e8d61b7bee0120e1838aa42
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-09-13 19:34:09 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -0,0 +1,21 @@
+def solution(N, stages):
+    answer = []
+    stage_probability = []
+    # 스테이지 마다 실패율 구하기
+    for i in range(1, N + 1):
+        count = 0
+        user = 0
+        for stage in stages:
+            if i <= stage:
+                user += 1
+                if i == stage:
+                    count += 1
+            if user == 0:
+                probability = 0
+            else:
+                probability = count / user
+        stage_probability.append((probability, i))
+
+    for m in sorted(stage_probability, key=lambda x: -x[0]):
+        answer.append(m[1])
+    return answer
