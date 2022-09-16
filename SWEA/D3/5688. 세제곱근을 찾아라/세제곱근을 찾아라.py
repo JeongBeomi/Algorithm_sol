2d4d06feaceda2dec7cd58028b0af9d3846ef071
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-09-16 10:50:33 +0900

    [D3] Title: 세제곱근을 찾아라, Time: 172 ms, Memory: 58,784 KB -BaekjoonHub

--- SWEA/D3/5688. 세제곱근을 찾아라/세제곱근을 찾아라.py
+++ SWEA/D3/5688. 세제곱근을 찾아라/세제곱근을 찾아라.py
@@ -1,13 +1,9 @@
 t = int(input())
 for tc in range(t):
-    n = int(input())
-    result = -1
-    i = 0
-    while True:
-        i += 1
-        if i ** 3 == n:
-            result = i
-            break
-        elif i ** 3 > n:
-            break
-    print(f"#{tc + 1} {result}")
+    n1 = int(input())
+    n2 = n1 ** (1/3)
+    print(f"#{tc + 1}", end=" ")
+    if round(n2) ** 3 == n1:
+        print(round(n2))
+    else:
+        print(-1)
