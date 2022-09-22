b2a0155ef5d4f4e9c49484df05e0cf444087445f
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-09-22 16:55:53 +0900

    [Silver I] Title: 회의실 배정, Time: 336 ms, Memory: 57760 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,5 +1,7 @@
-n = int(input())
-arr = [tuple(map(int, input().split())) for _ in range(n)]
+import sys
+
+n = int(sys.stdin.readline())
+arr = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
 arr.sort(key=lambda x: (x[1], x[0]))    # 작업 마감 시간으로 정렬, 동일한 마감시간 일시 먼저 시작하는 순으로 정렬
 cnt = 0
 t = 0               # 현재 시간
