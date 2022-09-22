4f1e6cfcb30f92f8af02c8c9a34ae9d5c27e4935
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-09-22 16:55:09 +0900

    [Silver I] Title: 회의실 배정, Time: 4320 ms, Memory: 51616 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = [tuple(map(int, input().split())) for _ in range(n)]
+arr.sort(key=lambda x: (x[1], x[0]))    # 작업 마감 시간으로 정렬, 동일한 마감시간 일시 먼저 시작하는 순으로 정렬
+cnt = 0
+t = 0               # 현재 시간
+for s, e in arr:    # 스케줄표 전체 탐색
+    if s >= t:      # 시작 시간이 현재 시간 보다 뒤 일때
+        t = e       # 해당 스케줄 끝나는 시간으로 현재시간 이동
+        cnt += 1
+
+print(f"{cnt}")
