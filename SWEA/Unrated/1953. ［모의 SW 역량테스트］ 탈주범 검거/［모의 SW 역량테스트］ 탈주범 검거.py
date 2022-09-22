78031b626d40995b5f4180afea6078ba0ec6108d
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-09-22 21:11:57 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 239 ms, Memory: 65,008 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.py
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.py
@@ -1,23 +1,22 @@
 from collections import deque
 
 
-
 def bfs(r, c):
     visited = [[False] * m for _ in range(n)]
     visited[r][c] = 1
-    cnt = 1
+    cnt = 1                 # 탈주범이 위치할 수 있는 장소의 개수
     q = deque([(r, c)])
     while q:
         r, c = q.popleft()
         for i in t_type[t_map[r][c]]:
             nr, nc = r + dr[i], c + dc[i]
+            # 인덱스 범위 안에 좌표가 존재하고, 길이 연결되어 있고, 방문한 적이 없으면 이동
             if 0 <= nr < n and 0 <= nc < m and (movable[i] in t_type[t_map[nr][nc]]) and not visited[nr][nc]:
                 visited[nr][nc] = visited[r][c] + 1
+                if visited[nr][nc] > l:     # 주어진 시간을 넘어서면 반환
+                    return cnt
                 cnt += 1
-                if visited[nr][nc] > l:
-                    return cnt - 1
                 q.append((nr, nc))
-                
     return cnt      
 
 # 상하좌우
