1fea4bedd189e9bb6cea14b169f576654ffb6e16
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-09-22 10:45:53 +0900

    [Silver IV] Title: 동전 0, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+result = 0
+for i in range(n - 1, -1, -1):
+    result += k // coins[i]
+    k %= coins[i]
+
+print(result)
