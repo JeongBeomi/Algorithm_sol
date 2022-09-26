74990c6e85e66e4078dc69bc5231ecd85d07cb27
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-09-26 20:00:05 +0900

    [Bronze I] Title: 색종이, Time: 1776 ms, Memory: 37624 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,13 +1,14 @@
-grid = [[0 for _ in range(1001)] for _ in range(1001)]
-num_of_papers = int(input())
+total_map = [[0] * 1001 for _ in range(1001)]
+n = int(input())
+min_r, max_r = 1001, 0
+for i in range(1, n + 1):
+    c, r, w, h = map(int, input().split())
+    for j in range(r, r + h):
+        total_map[j][c:c + w] = [i] * w
+    min_r, max_r = min(min_r, r), max(max_r, r + h)
 
-for p in range(1, num_of_papers+1):
-    p_x, p_y, width, height = map(int, input().split())
-    for y in range(p_y, p_y+height):
-        grid[y][p_x:(p_x+width)] = [p]*width
-            
-for p in range(1, num_of_papers+1):
+for p in range(1, n + 1):
     result = 0
-    for cnt in range(1001):
-        result += grid[cnt].count(p)
+    for c in range(min_r, max_r):
+        result += total_map[c].count(p)
     print(result)
