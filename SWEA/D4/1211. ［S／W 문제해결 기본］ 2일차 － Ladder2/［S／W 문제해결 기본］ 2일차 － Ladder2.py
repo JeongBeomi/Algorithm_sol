0e163dc1453b3374ea798aaaf1dca8791db64c07
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-09-29 01:46:30 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder2, Time: 393 ms, Memory: 67,900 KB -BaekjoonHub

--- SWEA/D4/1211. ［S／W 문제해결 기본］ 2일차 － Ladder2/［S／W 문제해결 기본］ 2일차 － Ladder2.py
+++ SWEA/D4/1211. ［S／W 문제해결 기본］ 2일차 － Ladder2/［S／W 문제해결 기본］ 2일차 － Ladder2.py
@@ -13,16 +13,17 @@ for _ in range(10):
         temp = copy.deepcopy(matrix)
         cnt = 0
         r, c = 0, s
-        temp[r][c] = 0
+        # temp[r][c] = 0
         while r < 99:
-            # temp[r][c] = 0
-            # cnt += 1
+            temp[r][c] = 0
+            cnt += 1
             for d in range(3):
                 nr, nc = r + dr[d], c + dc[d]
                 if 0 <= nr < 100 and 0 <= nc < 100 and temp[nr][nc] == 1:
                     r, c = nr, nc
-                    temp[r][c] = 0
-                    cnt += 1
+                    break
+                    # temp[r][c] = 0
+                    # cnt += 1
         cnt_list.append(cnt)
     result = cnt_list[::-1].index(min(cnt_list))
     print(f"#{n} {start[::-1][result]}")
