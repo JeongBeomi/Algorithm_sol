7075024dfcee15cbe0013946f6076bee86188b07
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-10-02 23:09:33 +0900

    [D2] Title: 숫자 배열 회전, Time: 117 ms, Memory: 56,792 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,17 +1,12 @@
-def turn(list_a):
-    result = list(map(list, zip(*list_a)))
-    for i in range(n):
-        result[i] = "".join(result[i])[::-1]
-    return result
-
-
 t = int(input())
-for tc in range(1, t + 1):
+for tc in range(t):
     n = int(input())
-    list_a = [list(input().split()) for _ in range(n)]
-    arr1 = turn(list_a)
-    arr2 = turn(arr1)
-    arr3 = turn(arr2)
-    print(f"#{tc}")
-    for i in range(n):
-        print(arr1[i], arr2[i], arr3[i])
+    matrix = [list(input().split()) for _ in range(n)]
+    result = [[] for _ in range(n)]
+    for _ in range(3):
+        matrix = list(zip(*matrix[::-1]))
+        for i in range(n):
+            result[i].append("".join(matrix[i]))
+    print(f"#{tc + 1}")
+    for j in range(n):
+        print(*result[j])
