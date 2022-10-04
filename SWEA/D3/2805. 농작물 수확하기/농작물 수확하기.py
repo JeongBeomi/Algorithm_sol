9184e44308ddaed5c285109273ad939874e6510c
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-10-04 23:22:54 +0900

    [D3] Title: 농작물 수확하기, Time: 200 ms, Memory: 62,732 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -2,10 +2,10 @@ t = int(input())
 for tc in range(t):
     n = int(input())
     total_map = [list(map(int, input())) for _ in range(n)]
-    m = n // 2
+    d = n // 2
     result = 0
     for i in range(n):
         for j in range(n):
-            if abs(m - i) + abs(m - j) <= m:
+            if abs(d - i) + abs(d - j) <= d:
                 result += total_map[i][j]
     print(f"#{tc + 1} {result}")
