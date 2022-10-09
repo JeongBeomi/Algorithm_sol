5c67a93f303db826ca9e9cf811e2610e866bdf8e
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-10-09 22:20:47 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 1,718 ms, Memory: 63,276 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
@@ -1,32 +1,18 @@
 t = int(input())
 for tc in range(t):
     n = int(input())
-    numbers_list = list(map(int, input().split()))
-    danjo = []
+    nums = list(map(int, input().split()))
+    max_v = -1
     for i in range(n - 1):
-        for j in range(i + 1, n):
-            num = numbers_list[i] * numbers_list[j]
-            if num <= 10:
-                break
-            num_list = []
-
-            while num:
-                num_list.append(num % 10)
-                num = num // 10
-
-            for k in range(len(num_list) - 1):
-                if num_list[k] < num_list[k + 1]:
+        for k in range(i + 1, n):
+            num = nums[i] * nums[k]
+            if max_v >= num:
+                continue
+            danjo = list(map(int, str(num)))
+            for j in range(len(danjo) - 1):
+                if danjo[j] > danjo[j + 1]:
                     break
             else:
-                danjo.append(numbers_list[i] * numbers_list[j])
-            # num_list = list(map(int, str(num)))
-            # for k in range(len(num_list) - 1):
-            #     if num_list[k] > num_list[k + 1]:
-            #         break
-            # else:
-            #     danjo.append(num)
+                max_v = num
 
-    if not danjo:
-        print(f"#{tc + 1} -1")
-    else:
-        print(f"#{tc + 1} {max(danjo)}")
+    print(f"#{tc + 1} {max_v}")
