ddde255b557d2684b597aad39a4d381862a01f7d
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-10-09 21:59:33 +0900

    [D3] Title: 민석이의 과제 체크하기, Time: 357 ms, Memory: 62,788 KB -BaekjoonHub

--- SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
+++ SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
@@ -1,9 +1,6 @@
 t = int(input())
 for tc in range(t):
     n, k = map(int, input().split())
-    member_list = list(map(int, input().split()))
-    result_list = []
-    for i in range(n):
-        if i + 1 not in member_list:
-            result_list.append(i + 1)
-    print(f"#{tc + 1}", *result_list)
+    people = set(range(1, n + 1))
+    person = set(map(int, input().split()))
+    print(f"#{tc + 1}", *sorted(people-person))
