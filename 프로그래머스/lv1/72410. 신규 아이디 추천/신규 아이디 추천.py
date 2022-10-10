c3dfea1c22b23604bfa81935182c06516d0f36b7
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-10-10 22:15:44 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.14 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,9 +1,4 @@
 def solution(new_id):
-    # if 3 <= len(new_id) <= 15 and new_id[0] != '.' and new_id[-1] != '.':
-    #     for s in new_id:
-    #         if s.isalpha():
-
-
     new_id = new_id.lower()
 
     for c in new_id:
@@ -11,12 +6,8 @@ def solution(new_id):
             continue
         new_id = new_id.replace(c, '')
 
-    i = 2
-    while i < len(new_id):
-        if new_id.find('.' * i) != -1:
-            new_id = new_id.replace("." * i, '.')
-            continue
-        i += 1
+    while '..' in new_id:
+        new_id = new_id.replace('..', '.')
 
     new_id = new_id.strip('.')
 
