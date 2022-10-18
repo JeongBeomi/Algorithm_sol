94870e12126878441d3fb731a0fe96840ec565b5
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-10-18 17:51:02 +0900

    [level 1] Title: 모의고사, Time: 4.12 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -0,0 +1,20 @@
+def solution(answers):
+    answer = []
+    fork = [(1, 2, 3, 4, 5), (2, 1, 2, 3, 2, 4, 2, 5), (3, 3, 1, 1, 2, 2, 4, 4, 5, 5)]
+    temp = []
+    for i in range(3):
+        cnt = 0
+        for j in range(len(answers)):
+            if answers[j] == fork[i][j % len(fork[i])]:
+                cnt += 1
+        temp.append((cnt, i + 1))
+    temp.sort(key=lambda x: (-x[0], x[1]))
+    print(temp)
+    for k in range(len(temp)):
+        cnt, idx = temp[k]
+        if not answer or temp[k - 1][0] == cnt:
+            answer.append(idx)
+        else:
+            break
+    return answer
+
