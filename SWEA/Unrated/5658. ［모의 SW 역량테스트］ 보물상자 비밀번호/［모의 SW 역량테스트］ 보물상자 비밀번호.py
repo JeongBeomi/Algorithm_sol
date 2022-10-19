a69fc858bc4b2713e0c36dba9879d2b46d38862c
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2022-10-19 23:06:11 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 109 ms, Memory: 48,732 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.py
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.py
@@ -6,7 +6,6 @@ for tc in range(t):
     for _ in range(n // 4):
         for i in range(0, n, n // 4):
             secret.add(int(nums[i:i + n // 4], 16))
-            # secret.add(nums[i:i + n // 4])
         nums = nums[1:n] + nums[0]
     result = sorted(list(secret), reverse=True)
     print(f'#{tc + 1} {result[k - 1]}')
