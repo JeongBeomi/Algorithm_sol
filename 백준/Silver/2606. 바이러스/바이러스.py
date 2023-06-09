8d26597a2d2d3759bcf61644dba031a343079601
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-06-09 18:40:21 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,33 +1,28 @@
-def dfs(v):
-    visited[v] = True         # 시작 정점 방문 처리
+from collections import deque
 
-    for next_v in graph[v]:
-        if not visited[next_v]: # 방문하지 않은 다음 인접 정점 탐색
-            global cnt
-            cnt += 1            # 감염 컴퓨터 증가
-            dfs(next_v)         # 다음 인접 정점으로 이동
+def dfs(start):
+    visted = [False] * (n + 1)
+    s = deque([start])
+    visted[start] = True
+    result = 0
 
+    while s:
+        v = s.pop()
+        for next_v in graph[v]:
+            if not visted[next_v]:
+                s.append(next_v)
+                visted[next_v] = True
+                result += 1
+    
+    return result
 
-# 정점 개수, 간선 개수
 n = int(input())
+graph = [[] for _ in range(n + 1)]
 m = int(input())
 
-# 그래프를 인접 리스트로 만들기 / 1~7을 인덱스 0~6으로 처리해서 -1
-graph = [[] for _ in range(n)]
 for _ in range(m):
     v1, v2 = map(int, input().split())
-    graph[v1-1].append(v2-1)
-    graph[v2-1].append(v1-1)
+    graph[v1].append(v2)
+    graph[v2].append(v1)
 
-# 그래프를 인접행렬로 만들기
-# graph = [[0] * n for _ in range(n)]
-# for _ in range(m):
-#     v1, v2 = map(int, input().split())
-#     graph[v1-1][v2-1] = 1
-#     graph[v2-1][v1-1] = 1
-
-visited = [False] * n   # 방문 처리 초기화
-cnt = 0
-
-dfs(0)      # 정점 1~7을 인덱스 0~6으로 정했기 때문에 함수 맨처음 값을 1-1=0을 넣음
-print(cnt)  # 정점을 0~7로 8개 만들어서 0은 간선이 없는 것으로 처리하는게 편할듯.
+print(dfs(1))
