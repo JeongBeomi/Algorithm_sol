9e9bf1509fb979743d482815d43e7c3122afc787
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-07-12 10:40:58 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,21 +1,6 @@
 from collections import deque
 
 def solution(k, score):
-#     num_list = deque(sorted(score[:k]))
-#     answer = [num_list[0] for _ in range(k)]
-#     # 일차 진행을 위한 반복문
-#     for i in range(k, len(score)):
-#         # 명예의 전당 순위 갱신
-#         for j in range(0, k):
-#             if score[i] < num_list[j]:
-#                 num_list.insert(j, score[i])
-#                 break
-#         else:
-#             num_list.append(score[i])
-            
-#         num_list.popleft()
-#         answer.append(num_list[0])
-
     answer = []
     num_list = []
     for num in score:
