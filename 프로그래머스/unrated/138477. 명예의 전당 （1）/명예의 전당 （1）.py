611737a5d62d5e0853754539c7a5a31a62d59f72
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-07-12 10:45:54 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,14 +1,12 @@
-from collections import deque
-
 def solution(k, score):
     answer = []
     num_list = []
+    
     for num in score:
         num_list.append(num)
         num_list.sort()
         if len(num_list) > k:
             num_list.pop(0)
-        
         answer.append(num_list[0])
             
     return answer
