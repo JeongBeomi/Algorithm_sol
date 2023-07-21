c1bdcda418d505c5f58f22bc62ebeca102574fb9
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-07-21 13:32:26 +0900

    [level 1] Title: 시저 암호, Time: 3.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -1,16 +1,33 @@
+# def solution(s, n):
+#     answer = ''
+#     upper_alpa = list(range(65, 91))
+#     lower_alpa = list(range(97, 123))
+    
+#     for ch in s:
+#         # 소문자
+#         if 97 <= ord(ch) <= 122:
+#             answer += chr(lower_alpa[(lower_alpa.index(ord(ch)) + n) % len(lower_alpa)])
+#         # 대문자
+#         elif 65 <= ord(ch) <= 90:
+#             answer += chr(upper_alpa[(upper_alpa.index(ord(ch)) + n) % len(upper_alpa)])
+#         # 공백
+#         else:
+#             answer += ch
+#     return answer
+
 def solution(s, n):
     answer = ''
-    upper_alpa = list(range(65, 91))
-    lower_alpa = list(range(97, 123))
     
-    for ch in s:
-        # 소문자
-        if 97 <= ord(ch) <= 122:
-            answer += chr(lower_alpa[(lower_alpa.index(ord(ch)) + n) % len(lower_alpa)])
-        # 대문자
-        elif 65 <= ord(ch) <= 90:
-            answer += chr(upper_alpa[(upper_alpa.index(ord(ch)) + n) % len(upper_alpa)])
-        # 공백
-        else:
-            answer += ch
+    for alpha in s:
+        alpha_i = ord(alpha)
+        # if alpha_i >= 65 and alpha_i < 91:      # 대문자
+        if 65 <= alpha_i < 91:      # 대문자
+            # answer += chr(((alpha_i+n)//91)*64 + (alpha_i+n)%90)
+            answer += chr(((alpha_i+n)//91)*65 + (alpha_i+n)%91)
+        # elif alpha_i >= 97 and alpha_i < 123:   # 소문자
+        elif 97<= alpha_i < 123:   # 소문자
+            answer += chr(((alpha_i+n)//123)*97 + (alpha_i+n)%123)
+        else:                                   # 공백
+            answer += " "
+    # print(ord('a')+25)
     return answer
