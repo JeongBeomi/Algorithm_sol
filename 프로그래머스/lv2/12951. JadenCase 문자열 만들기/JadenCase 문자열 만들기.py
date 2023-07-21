11512889cd6feed5dc74e192223edc2bbf5305f2
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-07-21 14:53:07 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,18 +1,7 @@
 def solution(s):
     answer = ''
-    word_list = list(s)
-    first_ch = 1
+    word_list = s.split(" ")
     for i in range(len(word_list)):
-        if word_list[i] == " ":
-            answer += word_list[i]
-            first_ch = 1
-        else:
-            if first_ch:
-                answer += word_list[i].upper()
-                first_ch = 0
-            else:
-                answer += word_list[i].lower()
-    # for i in range(len(word_list)):
-    #     word_list[i] = word_list[i].capitalize()
-    # answer = " ".join(word_list)
+        word_list[i] = word_list[i].capitalize()
+    answer = " ".join(word_list)
     return answer
