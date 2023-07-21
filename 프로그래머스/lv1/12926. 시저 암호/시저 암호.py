6dfcf52e4869f3205bbbbb4b64563fd59fecd5fa
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-07-21 09:20:27 +0900

    [level 1] Title: 시저 암호, Time: 3.90 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.py
@@ -2,12 +2,15 @@ def solution(s, n):
     answer = ''
     upper_alpa = list(range(65, 91))
     lower_alpa = list(range(97, 123))
-    #97~122, 65~90
+    
     for ch in s:
+        # 소문자
         if 97 <= ord(ch) <= 122:
             answer += chr(lower_alpa[(lower_alpa.index(ord(ch)) + n) % len(lower_alpa)])
+        # 대문자
         elif 64 <= ord(ch) <= 90:
             answer += chr(upper_alpa[(upper_alpa.index(ord(ch)) + n) % len(upper_alpa)])
+        # 공백
         else:
             answer += ch
     return answer
