2871798f8bcec9ac1697e8200da856ae741b14ae
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-07-24 16:46:24 +0900

    [unrated] Title: 공원 산책, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
@@ -1,28 +1,39 @@
+d = {
+    "N" : (-1, 0),
+    "S" : (1, 0),
+    "W" : (0, -1),
+    "E" : (0, 1)
+}
+
+def walking(park_map, route, now_pos):
+    direction, n = route.split()
+    n = int(n)
+    dr, dc = d[direction]
+		# 이동지점이 park 범위를 벗어나는지 확인
+    if 0 <= now_pos[0] + dr * n < len(park_map) and 0 <= now_pos[1] + dc * n < len(park_map[0]):
+				# 최종 이동지점이 park범위 안이니까 한칸씩 움직이며 가는길에 X가 있는지 확인해야 한다.
+        for i in range(1, n + 1):
+						# i에 따라 1칸 2칸 3칸 한칸씩 이동
+            nr, nc = now_pos[0] + dr * i, now_pos[1] + dc * i
+            if park_map[nr][nc] == "X":
+                break
+				# for문을 다돌았음에도 불구하고 X를 만나지못했다면 최종이동 지점까지 이동
+        else:
+            now_pos = [now_pos[0] + dr * n, now_pos[1] + dc * n]
+		# 다음 route 이동을 위해 현재 위치 반환
+    return now_pos
+    
+
 def solution(park, routes):
-    park = [list(p) for p in park]
-
-    # 방향
-    direction = 'NSWE'
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    t_x, t_y= 0, 0
-    for x in range(len(park)):
-        for y in range(len(park[x])):
-            if park[x][y] == 'S':          # 시작점 찾기
-                nx, ny = x, y
-                t_x, t_y = x, y
-
-                for route in routes:       # 이동 방향 더해주기
-                    op, n = route.split()  # d, n : 이동방향, 이동칸수
-                    i = direction.find(op)
-                    for _ in range(int(n)): # 이동칸수만큼 이동
-                        nx += dx[i] # 이동 
-                        ny += dy[i] # 이동
-
-                        if nx > len(park)-1 or ny > len(park[0])-1 or nx < 0 or ny < 0 or park[nx][ny] == 'X': # 장애물이 있을 때, 범위가 넘어갈때
-                            nx, ny = t_x, t_y
-                            break
-                    t_x, t_y = nx, ny  # x, y에 이동할 nx, ny를 넣어준다
-             
-
-    return [t_x, t_y]
+    s_pos = [0, 0]
+		# 시작점을 찾기 위해 park 순회
+    for row in range(len(park)):
+        col = park[row].find("S")
+        if col != -1:
+            s_pos = [row, col]
+            break
+    
+    for route in routes:
+				# 반환받은 위치로 위치 갱신
+        s_pos = walking(park, route, s_pos)
+    return s_pos
