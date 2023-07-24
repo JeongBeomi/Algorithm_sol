b9dc05aaffe4799cabe85db64cf6de2f2f0c56ae
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-07-24 15:39:59 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,19 +1,10 @@
 def solution(ingredient):
-    answer = 0
-    idx = 0
-    post_idx = -1
-    
-    while idx < len(ingredient) - 3:
-        if ingredient[idx : idx + 4] == [1, 2, 3, 1]:
-            answer += 1
-            # for _ in range(4):
-            #     ingredient.pop(idx)
-            del ingredient[idx:idx+4]
-            if idx >= 3:
-                idx -= 3
-            else:
-                idx = 0    
-        else: 
-            idx += 1
-        
-    return answer 
+    s = []
+    cnt = 0
+    for i in ingredient:
+        s.append(i)
+        if s[-4:] == [1, 2, 3, 1]:
+            cnt += 1
+            for i in range(4):
+                s.pop()
+    return cnt
