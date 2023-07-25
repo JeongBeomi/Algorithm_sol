9f0033611dded459420b0e5dadf575cb59a29019
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-07-25 10:12:19 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,9 @@
+def solution(players, callings):
+    answer = []
+    ranking_dict = {players[i] : i for i in range(len(players))}
+    for calling in callings:
+        idx = ranking_dict[calling]
+        players[idx], players[idx - 1] = players[idx - 1], players[idx]
+        ranking_dict[players[idx - 1]] -= 1
+        ranking_dict[players[idx]] += 1
+    return players
