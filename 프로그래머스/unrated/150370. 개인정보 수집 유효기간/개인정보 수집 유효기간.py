5ef22f81d0c22f23ad1a7a69bf19c1b0ec567aa5
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-07-26 10:41:44 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.22 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,27 +1,23 @@
 from datetime import date
 def solution(today, terms, privacies):
     answer = []
-    # 날짜비교를 위해 문자열 today를 date 객체로 변환
-    date_today = date.fromisoformat(today.replace(".", "-"))
-    terms_dict = {}
+    # today를 총 일수로 표현
+    # 연,월,일 int형으로 각각 저장
+    today_y, today_m, today_d = map(int, today.split("."))
+    today_total = today_y * 12 * 28 + today_m * 28 + today_d
     # 유효기간을 쉽게 찾기 위해 딕셔너리로 변환
+    terms_dict = {}
     for line in terms:
         name, term = line.split()
         terms_dict[name] = int(term)
     # 개인정보 리스트를 순회하며 파기해야하는지 확인
     for privacy_idx in range(len(privacies)):
         privacy_day, name = privacies[privacy_idx].split()
-        # 개인정보저장 날짜에 유효기간을 더하기 위해서 연,월,일 int형으로 각각 저장
+        # 연,월,일 int형으로 각각 저장
         privacy_y, privacy_m, privacy_d = map(int, privacy_day.split("."))
-        # 유효기간 더해주기
-        privacy_m += terms_dict[name] % 12
-        privacy_y += terms_dict[name] // 12
-        if privacy_m > 12:
-            privacy_m = (privacy_m % 13) + 1
-            privacy_y += 1
-        # 비교를 위해서 date 객체로 변환
-        date_privacy = date(privacy_y, privacy_m, privacy_d)
-        print(date_today, date_privacy)
-        if date_today >= date_privacy:
+        # 유효기간 더한 개인정보 유효기간 총 일수 구하기
+        privacy_total = privacy_y * 12 * 28 + privacy_m * 28 + privacy_d + terms_dict[name] * 28
+    
+        if today_total >= privacy_total:
             answer.append(privacy_idx + 1)
     return answer
