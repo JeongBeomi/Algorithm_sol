adeae902a0420ee0055d7911a1430ff2d2a218fc
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-07-27 09:08:00 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -2,5 +2,5 @@ def solution(food):
     answer = ''
     for i in range(1, len(food)):
         answer += str(i) * (food[i] // 2)
-    answer = answer + "0" + answer[-1 :: -1]
+    answer = answer + "0" + answer[:: -1]
     return answer
