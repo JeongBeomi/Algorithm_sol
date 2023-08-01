1e074101d5c055d7667a3ed75a2a09f37d808f91
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-08-01 09:44:39 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,6 @@
+def solution(brown, yellow):
+    answer = []
+    for i in range(1, int(yellow ** (1/2)) + 1):
+        if yellow % i == 0 and (i + 2) * (yellow // i + 2) == brown + yellow :
+            answer = [yellow // i + 2, i + 2]
+    return answer
