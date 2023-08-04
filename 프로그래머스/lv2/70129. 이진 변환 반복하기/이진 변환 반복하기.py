83f45e00ad5daf042dc13b34666f1fbefead440b
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-08-04 15:17:42 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.56 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    # 이진변환횟수, 제거한 0 수
+    answer = [0, 0]
+    while s != "1":
+        # 문자열에서 0제거
+        del_zero = "".join(s.split("0"))
+        # 이진변환수, 제거한 0 수 업데이트
+        answer[1] += len(s) - len(del_zero)
+        answer[0] += 1
+        # 이진 변환 결과를 s에 저장
+        s = str(bin(len(del_zero)))[2:]
+        
+    return answer
