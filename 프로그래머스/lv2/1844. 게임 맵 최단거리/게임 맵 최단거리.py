6d73cb71790221af285cf24ad8cd8c36eb91a7be
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-08-04 16:51:00 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -9,17 +9,15 @@ def solution(maps):
     q = deque([(0, 0, 1)])
     while q:
         r, c, d = q.popleft()
-        # 도착했는지 확인
-        if [r, c] == [n - 1, m - 1]:
-            answer = d
-            break
-            
         # 좌표이동
         for i in range(4):
             nr = r + dr[i]
             nc = c + dc[i]
             if 0 <= nr < n and 0 <= nc < m and maps[nr][nc]:
+                # 도착했는지 확인
+                if [nr, nc] == [n - 1, m - 1]:
+                    return d + 1
+                # 도착이 아니라면 다음으로 이동
                 q.append((nr, nc, d + 1))
                 maps[nr][nc] = 0
-    print(maps)
     return answer
