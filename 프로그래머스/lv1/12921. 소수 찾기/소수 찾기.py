e3d8594e293ddba1d340e158922d976b89135d14
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-08-14 09:36:38 +0900

    [level 1] Title: 소수 찾기, Time: 3371.28 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -21,10 +21,23 @@
 
 #     return memo[n]
 
-def solution(n):
-    num=set(range(2,n+1))
+# def solution(n):
+#     num=set(range(2,n+1))
 
-    for i in range(2,n+1):
-        if i in num:
-            num-=set(range(2*i,n+1,i))
-    return len(num)
+#     for i in range(2,n+1):
+#         if i in num:
+#             num-=set(range(2*i,n+1,i))
+#     return len(num)
+
+def solution(n):
+    answer = 0
+    for i in range(2, n+1):
+        if i == 2:
+            answer += 1
+            continue
+        for j in range(2, int(i ** (1/2)) + 1):
+            if i % j == 0:
+                answer -= 1
+                break
+        answer += 1
+    return answer
