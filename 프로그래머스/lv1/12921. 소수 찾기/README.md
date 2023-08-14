4de63e342327fad206f5f5e7e37200dffae744bc
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-08-14 09:33:48 +0900

    [level 1] Title: 소수 찾기, Time: 308.68 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/README.md
+++ 프로그래머스/lv1/12921. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 찾기 - 12921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921?language=python3) 
 
 ### 성능 요약
 
-메모리: 19.4 MB, 시간: 3035.30 ms
+메모리: 115 MB, 시간: 308.68 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,22 +1,30 @@
-# 인덱스 0 은버리고, 인덱스 1부터는 1부터 인덱스사이 수 소수 개수 
-memo = [0, 0]
-def solution(n):
-    answer = 0
-    cnt = len(memo)
-    # 지금까지 기록한 수 범위 안이면 바로 출력
-    if cnt >= n + 1:
-        return memo[n]
-    # 기록해놓은 범위 넘어가면 추가해주기
-    else:
-        for num in range(cnt, n + 1):
-            # 1가 본인 자신을 제외한 약수가 존재하지 않으면 소수
-            for i in range(2, int(num ** (1/2)) + 1):
-                # 2 ~ 해당숫자 ** 1/2 사이에서 하나라도 나누어떨이지면 소수가 아니다
-                if num % i == 0:
-                    memo.append(memo[num - 1])
-                    break
-            # 소수라면 전 갯수 + 1로 저장
-            else:
-                memo.append(memo[num - 1] + 1)
+# # 인덱스 0 은버리고, 인덱스 1부터는 1부터 인덱스사이 수 소수 개수 
+# memo = [0, 0]
+# def solution(n):
+#     answer = 0
+#     cnt = len(memo)
+#     # 지금까지 기록한 수 범위 안이면 바로 출력
+#     if cnt >= n + 1:
+#         return memo[n]
+#     # 기록해놓은 범위 넘어가면 추가해주기
+#     else:
+#         for num in range(cnt, n + 1):
+#             # 1가 본인 자신을 제외한 약수가 존재하지 않으면 소수
+#             for i in range(2, int(num ** (1/2)) + 1):
+#                 # 2 ~ 해당숫자 ** 1/2 사이에서 하나라도 나누어떨이지면 소수가 아니다
+#                 if num % i == 0:
+#                     memo.append(memo[num - 1])
+#                     break
+#             # 소수라면 전 갯수 + 1로 저장
+#             else:
+#                 memo.append(memo[num - 1] + 1)
 
-    return memo[n]
+#     return memo[n]
+
+def solution(n):
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
