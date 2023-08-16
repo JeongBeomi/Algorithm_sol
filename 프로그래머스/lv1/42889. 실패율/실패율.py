75f9dd3071009f407c9ab5d6e2e764e5b84311c3
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-08-16 10:22:26 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,24 +1,23 @@
 def solution(N, stages):
     answer = []
-    # 해당인덱스의 스테이지를 클리어한 인원수
-    clear_num = [0 for _ in range(N + 1)]
-    # stages 리스트를 순회하며 클리어 인원수 세기
-    for s in stages:
-        for i in range(0, s):
-            clear_num[i] += 1
-    temp = []
+    stage_user = len(stages)
     # 1 ~ N 까지 스테이지의 실패율 계산
+    temp = []
     for j in range(1, N + 1):
         # 실패율 계산
-        p = 0
-        if clear_num[j - 1] != 0:
-            p = 1 - clear_num[j] / clear_num[j - 1]
-        # (인덱스, 실패율) 실패율은 1 - (현재스테이지 클리어수) / (이전 스테이지 클리어수)
-        temp.append((j, p))
-    
+        fail_p = 0
+        if stage_user != 0:
+            cnt = stages.count(j)
+            fail_p = cnt / stage_user
+            stage_user -= cnt
+        temp.append((j, fail_p))
+
     # 정렬 후 answer에 스테이지만 순서대로 넣어주기
     temp.sort(key = lambda x : (-x[1], x[0]))
     for i in range(N):
         answer.append(temp[i][0])
         
     return answer
+
+
+
