f0ba1b5816da6fa7c963cf7051d4b4c4ff44e110
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-08-16 14:22:24 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,27 +1,39 @@
-def solution(nums):
-    answer = 0
-    n = len(nums)
-    max_sum = sum(sorted(nums)[n - 3 : n])
+# def solution(nums):
+#     answer = 0
+#     n = len(nums)
+#     max_sum = sum(sorted(nums)[n - 3 : n])
     
-    # 아리토스테네스의 체
-    check_set = set(range(max_sum + 1))
-    # 제일최근 값 ** 0.5 이후는 그전수의 배수로 생각안해도된다.
-    for i in range(2, int(max_sum ** (1/2) + 1)):
-        if i in check_set:
-            check_set -= set(range(2 * i, max_sum + 1, i))
+#     # 아리토스테네스의 체
+#     check_set = set(range(max_sum + 1))
+#     # 제일최근 값 ** 0.5 이후는 그전수의 배수로 생각안해도된다.
+#     for i in range(2, int(max_sum ** (1/2) + 1)):
+#         if i in check_set:
+#             check_set -= set(range(2 * i, max_sum + 1, i))
                              
-    # nums 중 세개 조합 combination 사용하면 더욱간단
-    num_sum = 0
-    for j in range(n - 2):
-        num_sum += nums[j]
-        for k in range(j + 1, n - 1):
-            num_sum += nums[k]
-            for m in range(k + 1, n):
-                num_sum += nums[m]
-                if num_sum in check_set:
-                    answer += 1
-                num_sum -= nums[m]
-            num_sum -= nums[k]
-        num_sum -= nums[j]
+#     # nums 중 세개 조합 combination 사용하면 더욱간단
+#     num_sum = 0
+#     for j in range(n - 2):
+#         num_sum += nums[j]
+#         for k in range(j + 1, n - 1):
+#             num_sum += nums[k]
+#             for m in range(k + 1, n):
+#                 num_sum += nums[m]
+#                 if num_sum in check_set:
+#                     answer += 1
+#                 num_sum -= nums[m]
+#             num_sum -= nums[k]
+#         num_sum -= nums[j]
     
+#     return answer
+
+def solution(nums):
+    from itertools import combinations as cb
+    answer = 0
+    for a in cb(nums, 3):
+        cand = sum(a)
+        for j in range(2, cand):
+            if cand%j==0:
+                break
+        else:
+            answer += 1
     return answer
