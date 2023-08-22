41ae49b0f5359e83b6bf468c4b58032806583880
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-08-22 09:40:26 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,15 +1,10 @@
 def solution(X, Y):
     answer = ''
-
-    # 작은 갯수로 추가해주기 
+    
     for i in range(9, -1, -1):
-        X = X.split(str(i))
-        Y = Y.split(str(i))
-        cnt = min(len(X), len(Y)) - 1
-        X = "".join(X)
-        Y = "".join(Y)
+        cnt = min(X.count(str(i)), Y.count(str(i)))
         answer += str(i) * cnt
-        
+    
     if not answer:
         answer = "-1"
     elif answer[0] == "0":
