fa3c3b095e0cab7e55a72bc14397d9a31de1e2d9
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-10-05 17:57:35 +0900

    [unrated] Title: 귤 고르기, Time: 23.67 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,17 +1,4 @@
-# def recur(cnt_dict, k, cnt, now_idx, answer):
-#     if answer[0] <= cnt:
-#         return
-    
-#     if k <= 0:
-#         answer[0] = min(answer[0], cnt)
-#         return
-    
-#     cnt_keys = list(cnt_dict.keys())
-#     for i in range(now_idx, len(cnt_dict)):
-#         recur(cnt_dict, k - cnt_dict[cnt_keys[i]], cnt + 1, i + 1, answer)
-
 def solution(k, tangerine):
-    sizes = list(set(tangerine))
     answer = 0
     cnt_dict = dict()
     
@@ -22,13 +9,15 @@ def solution(k, tangerine):
         else:
             cnt_dict[t] = 1
     
+    # 정렬을 위해 카운트 수 리스트 만들기
     cnt_list = list()
-    for i, v in cnt_dict.items():
-        cnt_list.append((i, v))
+    for v in cnt_dict.values():
+        cnt_list.append(v)
         
-    cnt_list.sort(key = lambda x : -x[1])
+    cnt_list.sort(reverse = True)
 
-    for t, cnt in cnt_list:
+    # 원하는 귤 개수 만큼 뽑기
+    for cnt in cnt_list:
         k -= cnt
         answer += 1
         if k <= 0:
