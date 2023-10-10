a89ed335c915823394ee0ba0604fb4a5fb11996a
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-10-10 22:53:38 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 264.30 ms, Memory: 43.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,12 @@
+def solution(elements):
+    answer = 0
+    n = len(elements)
+    num_set = set()
+    num_list = [0] * n
+    # 각 인덱스를 시작점으로 숫자를 하나씩 더해간다
+    for i in range(n):
+        for j in range(n):
+            num_list[j] += elements[(j + i) % n]
+            num_set.add(num_list[j])
+    answer = len(num_set)
+    return answer
