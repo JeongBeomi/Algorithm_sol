cf338655918eb98c3c5817ddcaa8939d715ced40
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-10-14 12:53:47 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 465.61 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -5,6 +5,8 @@ def solution(board):
         for j in range(1, m):
             if board[i][j] == 1:
                 # 정사각형의 크기는 현재위치에서 상, 좌, 좌상 좌표의 값의 영향을 받는다
+                # 본인 크기 1 + 가로세로 길이가 같아야하니 좌, 상 방향중 작은 값이 길이,
+                # 좌성 방향의 사각형은 같은 길이로 채워줘야한다
                 board[i][j] = 1 + min(board[i - 1][j], board[i - 1][j - 1], board[i][j - 1])
                 # 최대값 갱신
                 if answer < board[i][j]:
