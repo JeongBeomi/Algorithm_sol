d214f3412ffd58912b3498759bb2c0a4469892b3
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-10-17 10:55:56 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.13 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def solution(n, wires):
+    answer = n
+    graph = [[] for _ in range(n + 1)]
+    # 양방향 그래프
+    for wire in wires:
+        v1, v2 = wire
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+    
+    # 하나의 엣지가 끊어져있다면 두개의 정점중 하나의 정점의 자식정점 개수를 구해본다.
+    for wire in wires:
+        v1, v2 = wire
+        visited = [False] * (n + 1)
+        visited[v1], visited[v2] = True, True
+        q = deque([v1])
+        cnt = 1
+        while q:
+            v = q.popleft()
+            for nv in graph[v]:
+                if not visited[nv]:
+                    cnt += 1
+                    q.append(nv)
+                    visited[nv] = True
+                    
+        if abs(n - 2 * cnt) < answer:
+            answer = abs(n - 2 * cnt)
+    
+    return answer
