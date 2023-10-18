187adb63106473ee4e238c6ff4da726dd38675d4
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-10-18 20:16:17 +0900

    [level 2] Title: 괄호 변환, Time: 0.41 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,59 @@
+from collections import deque
+
+# 올바른 괄호인지 확인
+def check(u):
+    result = False
+    s = deque()
+    for i in range(len(u)):
+        if u[i] == ")":
+            if not s:
+                break
+            elif s[-1] == "(":
+                s.pop()
+                continue    
+        s.append(u[i])
+    else:
+        if not s:
+            result = True
+    
+    return result
+
+# 재귀
+def recur(w):
+    # 빈문자열이면 빈문자열 반환
+    if not w:
+        return ""
+    
+    w = deque(w)
+    u, v = "", ""
+    left_cnt, right_cnt = 0, 0
+    
+    # w를 u, v로 분리
+    for _ in range(len(w)):
+        temp = w.popleft()
+        if temp == "(":
+            left_cnt += 1
+        else:
+            right_cnt += 1
+        u += temp
+        if left_cnt == right_cnt:
+            v = "".join(w)
+            break
+    
+    if check(u):
+        return u + recur(v)
+    else:
+        temp = "(" + recur(v) + ")"
+        for c in u[1 : -1]:
+            if c == "(":
+                temp += ")"
+            else:
+                temp += "("
+        return temp
+        
+        
+def solution(p):
+    answer = ''
+    answer = recur(p)
+    
+    return answer
