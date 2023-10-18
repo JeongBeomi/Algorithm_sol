7a1314600f57a446268b974d09f5e68c155bff9d
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-10-18 21:17:18 +0900

    [level 2] Title: n^2 배열 자르기, Time: 27.46 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,6 +1,4 @@
 def solution(n, left, right):
-    answer = []
     # i를 통해 row, column을 알 수 있다. 해당자리는 r,c 중 최대값 + 1이 된다
-    for i in range(left, right + 1):
-        answer.append(max(i // n, i % n) + 1)
+    answer = [max(i // n, i % n) + 1 for i in range(left, right + 1)]
     return answer
