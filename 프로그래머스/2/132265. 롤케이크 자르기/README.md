7b218c997521e3d510d203363fb48d06d17a1e2f
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-10-19 10:32:05 +0900

    [level 2] Title: 롤케이크 자르기, Time: 296.07 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=python3) 
 
 ### 성능 요약
 
-메모리: 51.3 MB, 시간: 222.35 ms
+메모리: 51.4 MB, 시간: 296.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 10:29:17
+2023년 10월 4일 10:32:3
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,23 +1,25 @@
-from collections import Counter
-
 def solution(topping):
     answer = 0
     # 개수 비교를 위해 형은 추가만하면되기때문에 set, 동생은 감소를 해야하기 때문에 딕셔너리로 생성
     older = set()
-    younger = Counter(topping)
-    # for t in topping:
-    #     if t in younger:
-    #         younger[t] += 1
-    #     else:
-    #         younger[t] = 1
+    younger = dict()
+    for t in topping:
+        if t in younger:
+            younger[t] += 1
+        else:
+            younger[t] = 1
     
+    # 토핑을 하나씩 형쪽으로 넘기면서 개수가 동일할 때 answer += 1
     for t in topping:
         older.add(t)
         younger[t] -= 1
+        # 토핑이 0이되면 삭제
         if younger[t] == 0:
             del younger[t]
+        # 토핑 개수 비교
         if len(younger) == len(older):
             answer += 1
+        # 형이 더 커지면 그때부터는 같아질 수 없다
         elif len(younger) < len(older):
             break
     
