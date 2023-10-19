eeda63718a3713257fbd50f5e5baae794c2ecd89
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-10-19 10:29:19 +0900

    [level 2] Title: 롤케이크 자르기, Time: 222.35 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,12 +1,15 @@
+from collections import Counter
+
 def solution(topping):
     answer = 0
+    # 개수 비교를 위해 형은 추가만하면되기때문에 set, 동생은 감소를 해야하기 때문에 딕셔너리로 생성
     older = set()
-    younger = dict()
-    for t in topping:
-        if t in younger:
-            younger[t] += 1
-        else:
-            younger[t] = 1
+    younger = Counter(topping)
+    # for t in topping:
+    #     if t in younger:
+    #         younger[t] += 1
+    #     else:
+    #         younger[t] = 1
     
     for t in topping:
         older.add(t)
