497d97a570263dbe5a92ac5f883212636c695e2a
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-10-20 17:31:01 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,8 +1,7 @@
 def solution(citations):
     answer = 0
-    citations.sort()
+    citations.sort(reverse = True)
     for i in range(len(citations)):
-        if citations[i] >= len(citations) - i:
-            answer +=1
-
+        if citations[i] >= i + 1:
+            answer += 1
     return answer
