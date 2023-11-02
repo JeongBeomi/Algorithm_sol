65888d00080957c94a4a5f9716d23024f38a86f7
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-11-02 17:12:48 +0900

    [level 2] Title: [3차] 방금그곡, Time: 7.76 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -6,7 +6,6 @@ def check_melody(play_time, melody, m):
             if m[j - i] != melody[j % len(melody)]:
                 break
         else:
-            # if melody[(j + 1) % len(melody)] != "#":
             result = True
             break
     return result
@@ -14,19 +13,19 @@ def check_melody(play_time, melody, m):
 
 def solution(m, musicinfos):
     answer = ["(None)", 0]
+    # 음#을 한 문자로 대체
     replace_dict = {"C#" : "c", "D#" : "d", "F#" : "f", "G#" : "g", "A#" : "a"}
     for k, v in replace_dict.items():
         m = m.replace(k, v)
     
     for musicinfo in musicinfos:
         start_time, end_time, title, melody = musicinfo.split(",")
-        
+        # 음#을 한 문자로 대체
         for k, v in replace_dict.items():
             melody = melody.replace(k, v)
-            
-        # print(m, melody)
-        
+        # 플레이시간 계산
         play_time = (int(end_time[0:2]) - int(start_time[0:2])) * 60 + (int(end_time[3:5]) - int(start_time[3:5]))
+        # 동일 멜로디가 있고, 플레이 시간이 더 길면 answer 갱신
         if check_melody(play_time, melody, m) == True and answer[1] < play_time:
             answer = [title, play_time]
     
