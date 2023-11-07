424ad60af3bfd76267e375d3fa420d6316b82bc4
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-11-07 10:57:42 +0900

    [level 2] Title: 피로도, Time: 0.22 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,26 +1,19 @@
-a = 0
-
 def recur(k, dungeons, n, cnt, visited):
-    global a
     if n == len(dungeons):
-        print(cnt)
-        if a < cnt:
-            a = cnt
-        return
-    
+        return cnt
+    result = 0
     for i in range(len(dungeons)):
         if not visited[i]:
             visited[i] = 1
             if k >= dungeons[i][0]:
-                recur(k - dungeons[i][1], dungeons, n + 1, cnt + 1, visited)
+                result = max(result, recur(k - dungeons[i][1], dungeons, n + 1, cnt + 1, visited))
             else:
-                recur(k, dungeons, n + 1, cnt, visited)
+                result = max(result, recur(k, dungeons, n + 1, cnt, visited))
             visited[i] = 0
-    
+    return result
 
 def solution(k, dungeons):
     answer = -1
     visited = [0] * len(dungeons)
-    recur(k, dungeons, 0, 0, visited)
-    answer = a
+    answer = recur(k, dungeons, 0, 0, visited)
     return answer
