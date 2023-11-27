04f6166be8dd2e318c3eaedfeea7bc6f42913d89
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-11-27 18:15:05 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -3,22 +3,17 @@ def solution(n, k):
     total_cnt = 1
     num_list = list(range(1, n + 1))
     for i in range(1, n + 1):
-        total_cnt *= i    
+        total_cnt *= i
+    # 인덱스 연산을 위해 -1
     k = k - 1
+    
+    # 맨 앞자리 숫자부터 하나씩 찾을 예정
     while len(answer) < n:
+        # 하나의 자리수가 책임지는 경우의 수 개수는 만들 수 있는 전체 경우의 수 개수 // 원소 개수
         total_cnt = total_cnt // len(num_list)
+        # 몫이 사용하는 숫자의 인덱스
         answer.append(num_list.pop(k // total_cnt))
+        # 다음 숫자를 찾기 위해 k 갱신
         k = k % total_cnt
         
-    
     return answer
-'''
-0 1 2 3 4 5 / 6 7 8 9 10 11 / 12 13 14 15 16
-
-1 2 3 4  ..... 0
-1 2 4 3  ..... 1
-1 3 2 4  ..... 2
-1 3 4 2  ..... 3
-1 4 2 3  ..... 4
-1 4 3 2  ..... 5
-'''
