814e1ff54991ccb7d401bb18bcbde22a78854680
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2023-11-28 18:19:54 +0900

    [level unrated] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,10 +1,3 @@
 def solution(data, ext, val_ext, sort_by):
-    data_label = {"code" : 0, "date" : 1, "maximum" : 2, "remain" : 3}
-    
-    # data에서 ext 값이 val_ext보다 작은 데이터만 뽑기
-    data = [d for d in data if d[data_label[ext]] < val_ext]
-    
-    # 정렬
-    data.sort(key = lambda x : x[data_label[sort_by]])
-    
-    return data
+    answer = [[]]
+    return answer
