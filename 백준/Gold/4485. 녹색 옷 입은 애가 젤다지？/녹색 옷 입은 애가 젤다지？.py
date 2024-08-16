aeecdcb9d0fe66e8d450f9b445a91522065db5ed
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2024-08-16 16:32:44 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 136 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,36 +1,26 @@
-
 import heapq
 
-dr = (0, 0, -1, 1)
+dr = (0, 0, 1, -1)
 dc = (1, -1, 0, 0)
+idx = 0
 
-num = 1
 while True:
+    idx += 1
     n = int(input())
     if n == 0:
         break
 
-    cave = []
-    for _ in range(n):
-        cave.append(list(map(int, input().split())))
-
-    distance = [[n * n * 9] * n for _ in range(n)]
+    cave = [list(map(int, input().split())) for _ in range(n)]
+    distance = [[n * n * 10] * n for _ in range(n)]
     hq = [(cave[0][0], 0, 0)]
     distance[0][0] = cave[0][0]
 
     while hq:
-        min_dist, r, c = heapq.heappop(hq)
-        if min_dist > distance[r][c]:
-            continue
+        min_d, r, c = heapq.heappop(hq)
         for d in range(4):
             nr, nc = r + dr[d], c + dc[d]
-            if (
-                0 <= nr < n
-                and 0 <= nc < n
-                and distance[nr][nc] > min_dist + cave[nr][nc]
-            ):
-                distance[nr][nc] = cave[nr][nc] + min_dist
+            if 0 <= nr < n and 0 <= nc < n and distance[nr][nc] > min_d + cave[nr][nc]:
+                distance[nr][nc] = min_d + cave[nr][nc]
                 heapq.heappush(hq, (distance[nr][nc], nr, nc))
 
-    print("Problem {}: {}".format(num, distance[n - 1][n - 1]))
-    num += 1
+    print("Problem {}: {}".format(idx, distance[n - 1][n - 1]))
