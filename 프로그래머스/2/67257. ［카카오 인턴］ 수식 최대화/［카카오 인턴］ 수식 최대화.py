cc59f8a23e38cbb06196e96e8461ed42aabf285b
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2024-10-16 23:21:59 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.29 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,42 +1,31 @@
 from itertools import permutations
 from collections import deque
 
-cacul_dict = {
-    "*" : lambda x, y : x * y,
-    "+" : lambda x, y : x + y,
-    "-" : lambda x, y : x - y
+calcul_dict = {
+    "+": lambda x, y: x + y,
+    "-": lambda x, y: x - y,
+    "*": lambda x, y: x * y,
 }
 
-def cacul(per, e):
-    e = deque(e)
-    for p in per:
-        cnt = 0
-        n = len(e)
-        while cnt < n:
-            temp = e.popleft()
-            if temp == p:
-                cnt += 1
-                e.append(cacul_dict[temp](e.pop(), e.popleft()))
-            else:
-                e.append(temp)
-            cnt += 1
-    return abs(e[0])
-    
+
+def calcul(cnt, per_list, expression):
+    if cnt >= 3:
+        return int(expression)
+
+    p = per_list[cnt]
+    expression_list = expression.split(p)
+    result = calcul(cnt + 1, per_list, expression_list[0])
+
+    for idx in range(1, len(expression_list)):
+        result = calcul_dict[p](result, calcul(cnt + 1, per_list, expression_list[idx]))
+
+    return result
+
+
 def solution(expression):
     answer = 0
-    expression_list = []
-    temp = ""
-    for e in expression:
-        if e in ("*", "+", "-"):
-            expression_list.append(int(temp))
-            expression_list.append(e)
-            temp = ""
-            continue
-        temp += e
-    else:
-        expression_list.append(int(temp))
-
-    for per in permutations(["*", "+", "-"], 3):
-        answer = max(answer, cacul(per, expression_list))
-    
+    for per_list in permutations(["+", "-", "*"]):
+        calcul_num = abs(calcul(0, per_list, expression))
+        if answer < calcul_num:
+            answer = calcul_num
     return answer
