b3247a9c65abf0c63a42c916f500e293cfd10d82
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2025-07-09 16:16:58 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 104 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -2,34 +2,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.lang.StringBuilder;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-
-        Queue q = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            q.add(i + 1);
-        }
-
         StringBuilder sb = new StringBuilder("<");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < k - 1; j++) {
-                q.add(q.remove());
-            }
-            sb.append(q.remove());
-            if (i != (n - 1)) {
-                sb.append(", ");
-            }
+        
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        for (int i = 1; i <= n; i++) {
+            nums.add(i);
+        }
+        
+        int idx = 0;
+        while (!nums.isEmpty()) {
+            idx = (idx + k - 1) % nums.size();
+            sb.append(nums.remove(idx));
+            if (!nums.isEmpty()) sb.append(", ");
         }
         sb.append(">");
-        System.out.println(sb);
+        
+        System.out.println(sb.toString());
     }
 }
