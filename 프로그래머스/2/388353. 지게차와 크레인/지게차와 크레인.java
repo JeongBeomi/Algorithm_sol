10719003eb4caf76dd7f4ef3c5dd7162d5f6ea7f
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2025-07-17 16:50:25 +0900

    [level 2] Title: 지게차와 크레인, Time: 80.30 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,23 +1,23 @@
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Deque;
 import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
-    static int[][] storageMap;
-    static int r;
-    static int c;
-    static int[] dr = {0, 1, -1, 0};
-    static int[] dc = {1, 0, 0, -1};
+    static int N;
+    static int M;
+    static int[][] container;
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
     
-    public int crain(String request, String[] storage) {
+    public int crain (String[] storage, char c) {
         int cnt = 0;
         
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (request.charAt(0) == storage[i].charAt(j) && storageMap[i][j] == 0) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (container[i][j] == 0 && storage[i].charAt(j) == c) {
                     cnt++;
-                    storageMap[i][j] = 1;
+                    container[i][j] = 1;
                 }
             }
         }
@@ -25,68 +25,67 @@ class Solution {
         return cnt;
     }
     
-    public int lift(String request, String[] storage) {
+    public int lift (String[] storage, char c) {
         int cnt = 0;
-        ArrayList<int[]> changePos = new ArrayList<>();
+        ArrayList<int[]> changePos = new ArrayList<int[]>();
         
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (request.charAt(0) == storage[i].charAt(j) && storageMap[i][j] == 0) {
-                    if (bfs(i, j)) {
-                        cnt++;
-                        changePos.add(new int[]{i, j});
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (container[i][j] == 0 && storage[i].charAt(j) == c) {
+                    // bfs로 접근 가능한지 먼저 확인
+                    boolean accessible = false;
+                    ArrayList<int[]> temp = new ArrayList<int[]>();
+                    Deque<int[]> dq = new ArrayDeque<int[]>();
+                    boolean[][] visited = new boolean[N][M];
+                    dq.offer(new int[]{i, j});
+                    visited[i][j] = true;
+                    temp.add(new int[]{i, j});
+                    
+                    while(!dq.isEmpty()) {
+                        int[] pos = dq.poll();
+                        for (int d = 0; d < 4; d++) {
+                            int nr = pos[0] + dr[d], nc = pos[1] + dc[d];
+                            if (0 > nr || N <= nr || 0 > nc || M <= nc || container[nr][nc] == 2) {
+                                dq.clear();
+                                accessible = true;
+                                break;
+                            } else {
+                                if (!visited[nr][nc] && container[nr][nc] == 1) {
+                                    dq.offer(new int[]{nr, nc});
+                                    visited[nr][nc] = true;
+                                    temp.add(new int[]{nr, nc});
+                                }
+                            }
+                        }
+                    } 
+                    
+                    // 접근 가능하면 꺼내기
+                    if (accessible) {
+                        cnt ++;
+                        changePos.addAll(temp);
                     }
                 }
             }
         }
-        
-        for (int[] pos : changePos) {
-            int x = pos[0], y = pos[1];
-            storageMap[x][y] = 1;
+        for (int[] p : changePos) {
+            container[p[0]][p[1]] = 2;
         }
         
         return cnt;
-    }
-    
-    public boolean bfs(int i, int j) {
-        boolean result = false;
-        boolean[][] visited = new boolean[r][c];
-        Deque<int[]> dq = new ArrayDeque<>();
-        dq.offerLast(new int[]{i, j});
-        visited[i][j] = true;
-        
-        while (!dq.isEmpty()) {
-            int[] pos = dq.pollFirst();
-            int x = pos[0], y = pos[1];
-            for (int d = 0; d < 4; d++) {
-                int nx = dr[d] + x, ny = dc[d] + y;
-                if (0 <= nx && nx < r && 0 <= ny && ny < c) {
-                    if (storageMap[nx][ny] != 0 && !visited[nx][ny]) {
-                        visited[nx][ny] = true;
-                        dq.offerLast(new int[]{nx, ny});
-                    }
-                } else {
-                    result = true;
-                    dq = new ArrayDeque<>();
-                    break;
-                }
-            }
-        }
-        
-        return result;
     }
     
     public int solution(String[] storage, String[] requests) {
-        r = storage.length;
-        c = storage[0].length();
-        int answer = r * c;
-        storageMap = new int[r][c];
+        int answer = 0;
+        N = storage.length;
+        M = storage[0].length();
+        container = new int[N][M];
+        answer = N * M;
         
         for (String request : requests) {
-            if (request.length() == 1) {
-                answer -= lift(request, storage);
+            if (request.length() > 1) {
+                answer -= crain(storage, request.charAt(0));
             } else {
-                answer -= crain(request, storage);
+                answer -= lift(storage, request.charAt(0));
             }
         }
         
