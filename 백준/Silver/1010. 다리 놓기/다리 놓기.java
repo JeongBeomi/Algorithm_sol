33261489158314e4a8896424a73ebb2b73e49e63
JeongBeom Heo <109258397+JeongBeomi@users.noreply.github.com>
2025-07-18 14:38:45 +0900

    [Silver V] Title: 다리 놓기, Time: 120 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -4,32 +4,33 @@ import java.io.BufferedReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] bridgeCnt = new int[31][31];
+    static int N;
+    static int M;
+    static int[][] dp = new int[31][31];
     
-    public static int comb (int n, int m) {
-        if (bridgeCnt[n][m] != 0) {
-            return bridgeCnt[n][m];
+    public static int recur(int r, int c) {
+        if (dp[r][c] == 0) {
+            if (r == c || r == 0) {
+                dp[r][c] = 1;
+            } else {
+                dp[r][c] = recur(r - 1, c - 1) + recur(r, c - 1); 
+            }    
         }
-        if (n == 0 || n == m) {
-            bridgeCnt[n][m] = 1;
-            return 1;
-        }
-        int cnt = comb(n, m - 1) + comb(n - 1, m - 1);
-        bridgeCnt[n][m] = cnt;
-        return cnt;
+        return dp[r][c];
     }
     
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         StringTokenizer st;
         
-        for (int i = 0; i < t; i ++) {
+        for (int i = 0; i < t; i++) {
             st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
             
-            System.out.println(comb(n, m));
+            System.out.println(recur(N, M));
         }
+        
     }
 }
